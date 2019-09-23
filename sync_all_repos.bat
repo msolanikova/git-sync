9e61f84d17fe31dde5f91adf01b888fdb3c07402
Marketa.Solanikova <solanikova@promark.sk>
2019-09-23 10:42:53 +0200

    Windows batch script added

---
+++ sync_all_repos.bat
@@ -0,0 +1,8 @@
+@echo off
+set GIT_SYNC_RUNNER_PATH=c:\Development\GitRepos\private\gitsync\GitSyncRunner.py
+set DIR_TO_SYNC=c:\Development\GitRepos\
+set IGNORE_LIST_PATH=c:\Development\GitRepos\private\gitsync\blacklist.txt
+
+python %GIT_SYNC_RUNNER_PATH% --baseDir %DIR_TO_SYNC% --blacklistPath %IGNORE_LIST_PATH%
+
+set /p DUMMY=Hit ENTER to continue...
